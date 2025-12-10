c5f11c9f62c2bebd89926ed6e5f98c0c89eb9cab
ABDULRAFAY757 <112996960+ABDULRAFAY757@users.noreply.github.com>
2025-12-10 17:42:45 +0500

    Fix GitHub Pages paths

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+  output: 'export', // needed for static export
   trailingSlash: true,
+  basePath: '/rafay-portfolio', // your repo name
+  assetPrefix: '/rafay-portfolio/', // make sure to include the trailing slash
   images: {
     domains: ['github.com', 'avatars.githubusercontent.com', 'media.licdn.com'],
   },
