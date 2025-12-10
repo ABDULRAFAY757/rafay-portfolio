ddb2ca452cc36a3103864fccb06b50785f14fab3
ABDULRAFAY757 <112996960+ABDULRAFAY757@users.noreply.github.com>
2025-12-10 17:35:24 +0500

    Deploy updated site

--- next.config.js
+++ next.config.js
@@ -1,20 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     domains: ['github.com', 'avatars.githubusercontent.com', 'media.licdn.com'],
-//   },
-// }
-
-// module.exports = nextConfig
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
-  basePath: '/rafay-portfolio',
-  assetPrefix: '/rafay-portfolio',
   images: {
     domains: ['github.com', 'avatars.githubusercontent.com', 'media.licdn.com'],
   },
